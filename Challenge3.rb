d1158ad8b04dbf8ee08e51ca0e0c1eda8f2c3c91
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-03 16:55:12 +0100

    Added Challenge3

---
+++ Challenge3.rb
@@ -0,0 +1,5 @@
+require './First_challenge.rb'
+require './Challenge2.rb'
+def sort_and_stack (array)
+  arraystack(sort_array(array))
+end
--- First_challenge.rb
+++ First_challenge.rb
@@ -2,5 +2,3 @@ def arraystack (array)
   array.shift
   array.push(array[-1]+1)
 end
-
-puts arraystack([1,2,3,4,5])
